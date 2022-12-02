ac69db2c1c1f93f934bdf11ddcb6d2312c7f3901
kornel.zemla <kornel.zemla@ocado.com>
2022-12-02 11:25:35 +0100

    day 2

--- 002/script1.py
+++ 002/script1.py
@@ -1,5 +1,29 @@
 # test: 
-# input: 
+# input: 15572
 
-lines = open('input_test.txt', 'r').read().splitlines()
+lines = open('input.txt', 'r').read().splitlines()
 
+score = 0
+for l in lines:
+    oponent, me = l.split(' ')
+    print(f"{oponent}, {me}")
+    if me == 'X': 
+        score += 1
+        if oponent == 'C':
+            score += 6
+        if oponent == 'A':
+            score += 3
+    if me == 'Y': 
+        score += 2
+        if oponent == 'A':
+            score += 6
+        if oponent == 'B':
+            score += 3
+    if me == 'Z': 
+        score += 3
+        if oponent == 'B':
+            score += 6
+        if oponent == 'C':
+            score += 3
+    
+print(score)
--- 002/script2.py
+++ 002/script2.py
@@ -1,5 +1,40 @@
+import re
+
 # test: 
-# input: 
+# input: 16098
 
-lines = open('input_test.txt', 'r').read().splitlines()
+lines = open('input.txt', 'r').read().splitlines()
 
+
+def loose(oponent):
+    if oponent == 1: return 3
+    if oponent == 2: return 1
+    if oponent == 3: return 2
+
+
+def win(oponent):
+    if oponent == 1: return 2
+    if oponent == 2: return 3
+    if oponent == 3: return 1
+
+
+score = 0
+for l in lines:
+    l = re.sub("X|A", '1', l) # rock, loose
+    l = re.sub("Y|B", '2', l) # paper, tie
+    l = re.sub("Z|C", '3', l) # scissors, win
+
+    oponent, result = map(int, l.split(' '))
+
+    # loose
+    if result == 1:
+        score += loose(oponent)
+    # tie
+    if result == 2:
+        score += (3 + oponent)
+    # win
+    if result == 3:
+        score += (6 + win(oponent))
+    
+
+print(score)
