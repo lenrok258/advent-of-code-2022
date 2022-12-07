e84b71fc6c2747ab01149a466cd559a9fe3d52c2
kornel.zemla <kornel.zemla@ocado.com>
2022-12-07 10:21:06 +0100

    day 7

--- 007/script1.py
+++ 007/script1.py
@@ -1,9 +1,9 @@
 import re 
 
 # test: 
-# input: too low 756542
+# input: 1077191
 
-lines = open('input.txt', 'r').read().splitlines()
+lines = open('input.txt', 'r').read().splitlines()[1::]
 
 nodesDict = dict()
 
@@ -11,27 +11,22 @@ class Node:
     type = None # DIR, FILE
     size = None
     parent = None
+    path = "/"
 
-
-    def __init__(self, type, size, parent):
+    def __init__(self, type, size, name, parent):
         self.type = type
         self.size = int(size)
         self.parent = parent
-
+        if (self.parent != None):
+            self.path = self.parent.path + "/" + name
 
     def __str__(self):
-        return f"Node[{self.type, self.size, self.parent}]"
-
+        return f"Node[{self.type, self.size, self.path, self.parent}]"
 
     def increaseSize(self, sizeToAdd):
         self.size += sizeToAdd
 
-
     def updateSizes(self):
-        if self.type != "FILE":
-            return
-
-        # print(f"Update size {self}, parent {self.parent}")
         currentNode = self
         while True:
             currentNode.parent.increaseSize(self.size)
@@ -40,35 +35,26 @@ class Node:
                 break
 
 
-rootNode = Node("DIR", 0, None)
-nodesDict["/"] = rootNode
+rootNode = Node("DIR", 0, "", None)
+nodesDict[rootNode.path] = rootNode
 
 currentDir = rootNode
 for l in lines:
-    # print(f"Processing {l}")
-
     if l.startswith("dir"):
         dir_name = l.split(" ")[1]
-        nodesDict[dir_name] = Node("DIR", 0, currentDir)
+        node = Node("DIR", 0, dir_name, currentDir)
+        nodesDict[node.path] = node
     elif l == "$ cd ..":
         currentDir = currentDir.parent
     elif l.startswith("$ cd"):
         dir_name = l.split(" ")[2]
-        currentDir = nodesDict[dir_name] 
+        path = currentDir.path + "/" + dir_name
+        currentDir = nodesDict[path] 
     elif re.search("^\d", l):
-        # TODO: add full path as id (same name in multiple dirs)
         size, name = l.split(" ")
-        print(f"FILE {name}")
-        fileNode = Node("FILE", size, currentDir)
-        nodesDict[name] = fileNode
-        fileNode.updateSizes()
+        node = Node("FILE", size, name, currentDir)
+        nodesDict[node.path] = node
+        node.updateSizes()
 
-# for k, v in nodesDict.items():
-#     print(f"{k}\t\t{v}")
-
-summm = 0
-for k, node in nodesDict.items():
-    if node.type == "DIR" and node.size <= 100000:
-        summm += node.size
-
-print(summm)
+summ = sum([n.size for n in nodesDict.values() if n.type == 'DIR' and n.size <= 100000])
+print(summ)
--- 007/script2.py
+++ 007/script2.py
@@ -1,5 +1,66 @@
+import re 
+
 # test: 
-# input: 
+# input: 5649896
 
-lines = open('input_test.txt', 'r').read().splitlines()
+lines = open('input.txt', 'r').read().splitlines()[1::]
 
+nodesDict = dict()
+
+class Node:
+    type = None # DIR, FILE
+    size = None
+    parent = None
+    path = "/"
+
+    def __init__(self, type, size, name, parent):
+        self.type = type
+        self.size = int(size)
+        self.parent = parent
+        if (self.parent != None):
+            self.path = self.parent.path + "/" + name
+
+    def __str__(self):
+        return f"Node[{self.type, self.size, self.path, self.parent}]"
+
+    def increaseSize(self, sizeToAdd):
+        self.size += sizeToAdd
+
+    def updateSizes(self):
+        currentNode = self
+        while True:
+            currentNode.parent.increaseSize(self.size)
+            currentNode = currentNode.parent
+            if currentNode.parent == None: # root
+                break
+
+
+rootNode = Node("DIR", 0, "", None)
+nodesDict[rootNode.path] = rootNode
+
+currentDir = rootNode
+for l in lines:
+    if l.startswith("dir"):
+        dir_name = l.split(" ")[1]
+        node = Node("DIR", 0, dir_name, currentDir)
+        nodesDict[node.path] = node
+    elif l == "$ cd ..":
+        currentDir = currentDir.parent
+    elif l.startswith("$ cd"):
+        dir_name = l.split(" ")[2]
+        path = currentDir.path + "/" + dir_name
+        currentDir = nodesDict[path] 
+    elif re.search("^\d", l):
+        size, name = l.split(" ")
+        node = Node("FILE", size, name, currentDir)
+        nodesDict[node.path] = node
+        node.updateSizes()
+
+freeSpace = 70000000 - rootNode.size
+needed = 30000000 - freeSpace
+
+sorted_dirs = sorted([n.size for n in nodesDict.values() if n.type == "DIR"])
+for d in sorted_dirs:
+    if d > needed:
+        print(d)
+        break
